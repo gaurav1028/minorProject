3bd7b6e537ab7f52aaa8c5fa67330562b5b41e41
gaurav1028 <gauravpatyl28@gmail.com>
2021-03-16 21:15:59 +0530

    added few features

--- lib/screens/portal_screen.dart
+++ lib/screens/portal_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:minorProject/widgets/portal_screen/question_list.dart';
 
 class PortalScreen extends StatelessWidget {
+  void addQuestion() {}
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: addQuestion,
+          )
+        ],
+      ),
+      body: QuestionList(),
+    );
   }
 }
